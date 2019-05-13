7618d21205b2e0348a6da1980e820201d6072ead
Kostulski Marcin <marcin.kostulski@immalle.eu>
2019-05-13 10:27:17 +0200

    something but its fucking broken

--- JS/main.js
+++ JS/main.js
@@ -16,13 +16,11 @@ function myFunction() {
   }
 }
 
-//!!!!!!!!!!!!!
-//EXPERIMENTAL
 
-//function scrollto(element) {
+function scrollto(element) {
     // get the element on the page related to the button
-//    var scrollToId = element.getAttribute("data-scroll");
-//    var scrollToElement = document.getElementById(scrollToId);
+   var scrollToId = element.getAttribute("data-scroll");
+   var scrollToElement = document.getElementById(scrollToId);
     // make the page scroll down to where you want
     // ...
-//}
+}
